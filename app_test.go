d0bc43f5366aa46239e6f9c9ef314fdf507d2bab
Adam Wilson <adam.wilson@sky.uk>
2021-02-08 15:54:59 +0000

    tests: add test skeleton mock structs

--- app_test.go
+++ app_test.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"database/sql"
+)
+
+type MockDB struct {
+	callParams []interface{}
+}
+
+func (mdb *MockDB) Exec(query string, args ...interface{}) (*sql.Result, error) {
+	return nil, nil
+}
+
+func (mdb *MockDB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return nil, nil
+}
+
+func (mdb *MockDB) QueryRow(query string, args ...interface{}) *sql.Row {
+	return nil
+}
+
+type MockResult struct {
+	InsertId int64
+	Rows int64
+}
+
+func (mr *MockResult) LastInsertId() (int64, error) {
+	return mr.InsertId, nil
+}
+
+func (mr *MockResult) RowsAffected() (int64, error) {
+	return mr.Rows, nil
+}
