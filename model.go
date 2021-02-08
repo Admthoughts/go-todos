d3146f3a68ab99180e8b6a6347585492f3a2bfa6
Adam Wilson <adam.wilson@sky.uk>
2021-02-08 15:55:17 +0000

    refactor: add interfaces for mocking

--- model.go
+++ model.go
@@ -29,6 +29,17 @@ type Response struct {
 	Error     `json:"error,omitempty"`
 }
 
+type SQLDB interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+}
+
+type SQLResult interface {
+	LastInsertId() (int64, error)
+	RowsAffected() (int64, error)
+}
+
 func writeResponse(res Response, w *http.ResponseWriter) {
 	bytes, err := json.Marshal(res)
 	if err != nil {
@@ -39,7 +50,7 @@ func writeResponse(res Response, w *http.ResponseWriter) {
 	fmt.Fprintf(*w, "%s\n", bytes)
 }
 
-func (t *Todo) newTodo(db *sql.DB) error {
+func (t *Todo) newTodo(db SQLDB) error {
 	err := db.QueryRow(
 		"INSERT INTO todos(text, done, createdon, updatedon) VALUES($1, $2, current_timestamp, current_timestamp) RETURNING id",
 		t.Text, t.Done).Scan(&t.ID)
@@ -49,12 +60,12 @@ func (t *Todo) newTodo(db *sql.DB) error {
 	return nil
 }
 
-func (t *Todo) getTodo(db *sql.DB) error {
+func (t *Todo) getTodo(db SQLDB) error {
 	return db.QueryRow("SELECT text, done, CreatedOn FROM todos WHERE id=$1", t.ID).Scan(&t.Text,
 		&t.Done, &t.CreatedOn)
 }
 
-func getTodos(db *sql.DB, start, count int) ([]Todo, error) {
+func getTodos(db SQLDB, start, count int) ([]Todo, error) {
 	rows, err := db.Query(
 		"SELECT id, text, done, CreatedOn FROM todos LIMIT $1 OFFSET $2",
 		count, start)
@@ -77,13 +88,13 @@ func getTodos(db *sql.DB, start, count int) ([]Todo, error) {
 
 }
 
-func (t *Todo) updateTodo(db *sql.DB) error {
+func (t *Todo) updateTodo(db SQLDB) error {
 	_, err := db.Exec(
 		"UPDATE todos SET text=$1, done=$2, UpdatedOn=current_timestamp WHERE id=$3", t.Text, t.Done, t.ID)
 	return err
 }
 
-func (t *Todo) deleteTodo(db *sql.DB) error {
+func (t *Todo) deleteTodo(db SQLDB) error {
 	_, err := db.Exec(
 		"DELETE FROM todos WHERE id=$1", t.ID)
 	return err
